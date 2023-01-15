4b9e917bf6806ffc02b2fa07dde88fba34a56ec4
Ubinquitous <ubinquitous1@gmail.com>
2023-01-15 19:50:08 +0900

    ADD : HomeScreen 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dadamum/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
